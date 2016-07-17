35406ac143f4af3439ee2779813d6f66ba623819
Scott C Reynolds <scott@scottcreynolds.com>
2016-07-17 18:31:54 -0400

    first draft (#1)
    * solution first draft
    
    * update readme
    
    * first draft
    
    * fix test

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much.";
+function getIssues() {
+}
+
+function showIssues(json) {
+}
+
+function createIssue() {
+}
+
+function showResults(json) {
+}
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  //use fetch to fork it!
+}
+
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return ''
+}
