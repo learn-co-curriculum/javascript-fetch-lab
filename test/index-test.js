a7b431711bb936b57b3381e3868f3b2ab16e7e72
Brian Holland <beehollander@gmail.com>
2016-10-25 22:34:33 -0500

    add case insensativity; 'url' -> 'html_url'

--- test/index-test.js
+++ test/index-test.js
@@ -32,7 +32,7 @@ describe('index', () => {
         const temp = document.getElementById('issues-template').innerHTML
         expect(temp).toMatch(/{{#\s?each/)
         expect(temp).toMatch(/{{\/\s?each/)
-        expect(temp).toMatch(/{{\s?url\s?}}/)
+        expect(temp).toMatch(/{{\s?html_url\s?}}/)
         expect(temp).toMatch(/{{\s?body\s?}}/)
         expect(temp).toMatch(/{{\s?title\s?}}/)
       })
@@ -80,7 +80,7 @@ describe('index', () => {
       const url = fetchSpy.calls[0].arguments[0]
       expect(url).toMatch(/api.github.com\/repos\/learn-co-curriculum\/javascript-fetch-lab/)
       const opts = fetchSpy.calls[0].arguments[1]
-      expect(opts.method).toMatch(/post/)
+      expect(opts.method).toMatch(/post/i)
       expect(opts.headers).toMatch(/Authorization: token\s./)
     })
 
@@ -93,7 +93,7 @@ describe('index', () => {
       expect(url).toMatch(/javascript-fetch-lab\/issues/)
       expect(url).toNotMatch(/learn-co-curriculum/)
       const opts = fetchSpy.calls[0].arguments[1]
-      expect(opts.method).toMatch(/post/)
+      expect(opts.method).toMatch(/post/i)
       expect(opts.headers).toMatch(/Authorization: token\s./)
       expect(opts.body).toMatch(/test body/)
     })
